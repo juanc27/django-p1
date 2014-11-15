a85e9b1a3cebcd08f129188ef1677e4e66380440
Juan <juanc.munozv@gmail.com>
2014-11-14 19:52:44 -0800

    news with default values

--- myfavteam/views.py
+++ myfavteam/views.py
@@ -3,6 +3,43 @@ from myfavteam.models import *
 import datetime
 import copy
 
+# Create your views here.
+def index(request, team = -1):
+    resp_dict = dict()
+
+    #get my fav teams
+    total_my_favteams = Team.objects.filter(my_team=True).count()
+    teams = Team.objects.filter(my_team=True)
+    resp_dict['teams'] = teams
+
+    #pics for carousel
+    c = Carousel()
+    c.load_data(resp_dict)
+    
+    #news
+    n = NewsList()
+    news = n.get_news(5)
+    resp_dict['latest_news'] = news[0]
+    resp_dict['news'] = news
+    
+    #next games
+    g = Games()
+    next_games = g.get_next_games(2) 
+    resp_dict['next_game'] = next_games[0] 
+    resp_dict['next_games']= next_games
+
+    #last games
+    last_games = g.get_last_games(2)
+    resp_dict['last_game'] = last_games[0]      
+    resp_dict['last_games']= last_games
+
+    return render(request, 'myfavteam/index.html', resp_dict)
+
+def news(request):
+    #get my news
+    news = News.objects.all()
+    return render(request, 'myfavteam/news.html', {'news':news})
+
 class Carousel:
     '''Object to build all data, task for the carousel on index.html'''
     def __init__(self):
@@ -29,18 +66,18 @@ class Games:
     '''Object to build all data, task for the carousel on index.html'''
     def __init__(self):
         now = datetime.datetime.now()
-        self.next_games = [{'team': 'MyFavTeam', 'team_against': 'Opponent1', 
-                            'stadium' : {'name': 'Our Stadium', 'city': 'Our City'}, 
+        self.next_games = [{'team': 'MyFavTeam', 'team_against': 'Opponent1',
+                            'stadium' : {'name': 'Our Stadium', 'city': 'Our City'},
                             'tournament': 'Tournament A',
                             'home': True, 'date' : now.replace(year=now.year+1),
-                            'team_score': 50, 'against_score': 49, 
+                            'team_score': 50, 'against_score': 49,
                            },
-                           {'team': 'MyFavTeam', 'team_against': 'Opponent2', 
-                            'stadium' : {'name': 'Stadium', 'city': 'City'}, 
+                           {'team': 'MyFavTeam', 'team_against': 'Opponent2',
+                            'stadium' : {'name': 'Stadium', 'city': 'City'},
                             'tournament': 'Tournament A',
                             'home': False, 'date' : now.replace(year=now.year+2),
                             'team_score': 99, 'against_score': 100,
-                           } 
+                           }
                           ]
         self.last_games = copy.deepcopy(self.next_games)
         self.last_games.reverse()
@@ -56,7 +93,7 @@ class Games:
         else:
             query = Schedule.objects.filter(date__lt=now)
             ret_list = list(self.last_games)
-        
+
         count = query.count()
         if count < amount:
             amount = count
@@ -72,49 +109,40 @@ class Games:
     def get_last_games(self, amount):
         return self.get_games(amount, next=False)
 
-# Create your views here.
-def index(request, team = -1):
-    resp_dict = dict()
+class NewsList:
+    '''Object to build all data, task for the carousel on index.html'''
+    def __init__(self):
+        now = datetime.datetime.now()
+        now2 = now.replace(minute=now.minute-10)
+            
+        self.news = [{'team': 'MyFavTeam',
+                      'title': 'News1: MyFavTeam App collectis news from different web sources',
+                      'date' : now,
+                      'link' : '#',
+                      'author': 'myself',
+                      'website': 'website1.com',
+                      'image': {'url': 'holder.js/110x62/auto/#666:#999/text: image or logo'},
+                     },
+                     {'team': 'MyFavTeam',
+                    'title': 'News2: MyFavTeam App also collectis stats from a trusted web source',
+                      'date' : now2,
+                      'link' : '#',
+                      'author': 'myself2',
+                      'website': 'website2.com',
+                      'image': {'url': 'holder.js/110x62/auto/#555:#888/text: image or logo'},
+                     },
+                    ]
 
-    #get my fav teams
-    total_my_favteams = Team.objects.filter(my_team=True).count()
-    teams = Team.objects.filter(my_team=True)
-    resp_dict['teams'] = teams
+    def get_news(self, amount):
+        query = News.objects.order_by('-date')
+        ret_list = list(self.news)
 
-    #pics for carousel
-    c = Carousel()
-    c.load_data(resp_dict)
-    
-    #news
-    total_news = News.objects.count()
-    if total_news > 9:
-        total_news = 10
-    resp_dict['total_news'] = total_news
-    
-    news = News.objects.order_by('-date')[0:total_news]
-    resp_dict['news'] = news
-    if total_news > 0:
-        latest_news = News.objects.order_by('-date')[0]
-    else:
-        latest_news = False
-    resp_dict['latest_news'] = latest_news  
-    
-    #next games
-    a = Games()
-    next_games = a.get_next_games(2) 
-    resp_dict['next_game'] = next_games[0] 
-    resp_dict['next_games']= next_games
-
-    #last games
-    a = Games()
-    last_games = a.get_last_games(2)
-    resp_dict['last_game'] = last_games[0]      
-    resp_dict['last_games']= last_games
-
-    return render(request, 'myfavteam/index.html', resp_dict)
-
-def news(request):
-    #get my news
-    news = News.objects.all()
-    return render(request, 'myfavteam/news.html', {'news':news})
+        count = query.count()
+        if count < amount:
+            amount = count
+        if amount >= len(ret_list):
+            return query[0:amount]
+        else:
+            ret_list[0:amount] = query[0:amount]
+            return ret_list
 
